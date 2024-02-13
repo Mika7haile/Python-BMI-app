73aeb2653b20840fbbacbb5628404ee9018bced7
Hailemicheal Eshetu <haile7mika@gmail.com>
2024-02-13 19:41:08 +0300

    last commit, now to layout and widgets

--- bmi.py
+++ bmi.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
 from settings import *
-from ctypes import windll, byref, sizeof, c_int
-
+try:
+    from ctypes import windll, byref, sizeof, c_int
+except:
+    pass
 
 class App(ctk.CTk):
     def __init__(self):
@@ -12,16 +14,24 @@ class App(ctk.CTk):
         self.iconbitmap('bmi.ico')
         self.geometry('400x400')
         self.resizable(False, False)
-        self.change_title_bar_color()
-        self.mainloop()
-    def change_title_bar_color(self):
-        HWND = windll.user32.GetParent(self.winfo_id())
-        title_bar_color = 0x0000FF00
-        windll.dwmapi.DwmSetWindowAttribute(
-            HWND,
-            35,
-            byref(c_int(title_bar_color)),
-            sizeof(c_int)
-        )        
+        self.change_title_bar_color(TITLE_HEX_COLOR)
 
-App()
+        # layout with grid
+
+        self.mainloop()
+    def change_title_bar_color(self, HexColore):
+        try:
+            HWND = windll.user32.GetParent(self.winfo_id())
+            title_bar_color = HexColore
+            windll.dwmapi.DwmSetWindowAttribute(
+                HWND,
+                35,
+                byref(c_int(title_bar_color)),
+                sizeof(c_int)
+        )        
+        except: 
+            pass
+
+
+if __name__ == '__main__':
+    App()
