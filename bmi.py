4508368782f4e32994cc4761054c539979b892e7
Hailemicheal Eshetu <haile7mika@gmail.com>
2024-02-13 23:20:56 +0300

    Inpute frame is completed with buttons and labels populated

--- bmi.py
+++ bmi.py
@@ -21,8 +21,10 @@ class App(ctk.CTk):
         self.rowconfigure((0,1,2,3), weight = 1, uniform='as')
         # widgets
         ResultText(self)
-
+        weightInput(self)
         self.mainloop()
+        
+
     def change_title_bar_color(self, HexColore):
         try:
             HWND = windll.user32.GetParent(self.winfo_id())
@@ -44,6 +46,69 @@ class ResultText(ctk.CTkLabel):
             font = font,
             text_color = WHITE)
         self.grid(row = 0, column = 0, rowspan = 2, sticky = 'nwes')
+class weightInput(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, fg_color= WHITE)
+        self.grid(row = 2, column = 0, sticky = 'nsew', padx = 10, pady = 10)
+
+        # layout 
+        self.rowconfigure(0, weight= 1, uniform= "b")
+        self.columnconfigure(0, weight= 2, uniform= "b")
+        self.columnconfigure(1, weight= 1, uniform= "b")
+        self.columnconfigure(2, weight= 3, uniform= "b")
+        self.columnconfigure(3, weight= 1, uniform= "b")
+        self.columnconfigure(4, weight= 2, uniform= "b")
+        
+        # widgets
+        
+        # label
+        font = ctk.CTkFont(family= FONT, size= INPUT_FONT_SIZE)
+        label = ctk.CTkLabel(self, text= '34', text_color= BLACK, font= font)
+        label.grid(row = 0, column = 2)
+        # buttons
+        minus_button = ctk.CTkButton(
+            self,
+            text= '-',
+            fg_color=LIGHT_GRAY,
+            font= font,
+            text_color=BLACK,
+            hover_color= GRAY,
+            corner_radius= BUTTON_CORNER_RADIUS
+            )
+        minus_button.grid(row = 0, column = 0, sticky = 'ns', padx = 8, pady = 8)
+        small_minus_button = ctk.CTkButton(
+            self,
+            text= '-',
+            fg_color=LIGHT_GRAY,
+            font= font,
+            text_color=BLACK,
+            hover_color= GRAY,
+            corner_radius= BUTTON_CORNER_RADIUS
+            )
+        small_minus_button.grid(row = 0, column = 1, padx = 4, pady = 4)
+        plus_button = ctk.CTkButton(
+            self,
+            text= '+',
+            fg_color=LIGHT_GRAY,
+            font= font,
+            text_color=BLACK,
+            hover_color= GRAY,
+            corner_radius= BUTTON_CORNER_RADIUS
+            )
+        plus_button.grid(row = 0, column = 4, sticky = 'ns', padx = 8, pady = 8)
+        small_plus_button = ctk.CTkButton(
+            self,
+            text= '+',
+            fg_color=LIGHT_GRAY,
+            font= font,
+            text_color=BLACK,
+            hover_color= GRAY,
+            corner_radius= BUTTON_CORNER_RADIUS
+            )
+        small_plus_button.grid(row = 0, column = 3, padx = 4, pady = 4)
+
 
 if __name__ == '__main__':
     App()
+
+    
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ FONT = 'Calibri'
 MAIN_TEXT_SIZE = 150
 INPUT_FONT_SIZE = 26
 SWITCH_FONT_SIZE = 18
-
+BUTTON_CORNER_RADIUS = 6
 # Colores
 
 GREEN = '#50BFAB'
@@ -17,53 +17,3 @@ GRAY = '#D9D9D9'
 
 TITLE_HEX_COLOR = 0x00ABBF50 
 
-"""import customtkinter as ctk
-from settings import *
-try:
-    from ctypes import windll, byref, sizeof, c_int
-except:
-    pass
-
-class App(ctk.CTk):
-    def __init__(self):
-
-        # window setup
-        super().__init__(fg_color= GREEN)
-        self.title('')
-        self.iconbitmap('bmi.ico')
-        self.geometry('400x400')
-        self.resizable(False, False)
-        self.change_title_bar_color(TITLE_HEX_COLOR)
-
-        # layout
-        self.columnconfigure(0, weight = 1)
-        self.rowconfigure((0,1,2,3), weight = 1, uniform='as')
-        # widgets
-        ResultText(self)
-        weightInput(self)
-
-        self.mainloop()
-    def change_title_bar_color(self, HexColore):
-        try:
-            HWND = windll.user32.GetParent(self.winfo_id())
-            title_bar_color = HexColore
-            windll.dwmapi.DwmSetWindowAttribute(
-                HWND,
-                35,
-                byref(c_int(title_bar_color)),
-                sizeof(c_int)
-        )        
-        except: 
-            pass
-
-
-    
-class weightInput(ctk.CTkFrame):
-    def __init__(self, parent):
-        super().__init__(parent, fg_color= WHITE)
-        self.grid(row = 2, column = 0, sticky = 'nsew', padx = 10, pady = 10)
-
-        
-
-if __name__ == '__main__':
-    App()"""
