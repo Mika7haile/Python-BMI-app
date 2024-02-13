39be2c1d83630fd4509c755906270d913e77078e
Hailemicheal Eshetu <haile7mika@gmail.com>
2024-02-13 19:29:03 +0300

    Wrote a function to change the title bar colore

--- bmi.py
+++ bmi.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from settings import *
+from ctypes import windll, byref, sizeof, c_int
 
 
 class App(ctk.CTk):
@@ -7,6 +8,20 @@ class App(ctk.CTk):
 
         # window setup
         super().__init__(fg_color= GREEN)
-
+        self.title('')
+        self.iconbitmap('bmi.ico')
+        self.geometry('400x400')
+        self.resizable(False, False)
+        self.change_title_bar_color()
         self.mainloop()
+    def change_title_bar_color(self):
+        HWND = windll.user32.GetParent(self.winfo_id())
+        title_bar_color = 0x0000FF00
+        windll.dwmapi.DwmSetWindowAttribute(
+            HWND,
+            35,
+            byref(c_int(title_bar_color)),
+            sizeof(c_int)
+        )        
+
 App()
